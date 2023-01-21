bc25c46d4ce42c6ea7015bde17fcc5c3781a84f9
Reinaldo J <reinaldo@bloopark.de>
2023-01-21 06:52:28 +0100

    [ADD] Extract text from Image
    OCR is performed in the given image on /upload endpoint using
    tesseract-ocr.
    
    More info about this package on
    https://github.com/tesseract-ocr/tesseract

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import logging
+from PIL import Image
+from pytesseract import pytesseract
 
 from flask import Flask, request
 
@@ -14,11 +16,35 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    file = request.files['file']
-    filename = file.filename
-    file.save('./uploads/' + filename)
-    logging.debug(f'File {filename} saved to uploads folder.')
-    return {'message': 'File uploaded.'}
+    try:
+        file = request.files['file']
+        # TODO Extends to PDFs. For now, file will be considered as an image.
+        path = f'./uploads/{file.filename}'
+        file.save(path)
+        logging.debug(f'File saved to {path}.')
+        text = extract_text_from_image(path)
+        logging.debug('File processed.')
+        return {
+            'text': text,
+            'message': 'File uploaded.',
+        }
+    except Exception as e:
+        return {
+            'internal_error': f'{e}',
+        }
+
+
+def extract_text_from_image(path):
+    """Extract text from image in the given path.
+
+    Args:
+        path (str): path to the image to be processed.
+    Returns:
+        extracted text from the image
+    """
+    image = Image.open(path)
+    text = pytesseract.image_to_string(image)
+    return text
 
 
 if __name__ == '__main__':
