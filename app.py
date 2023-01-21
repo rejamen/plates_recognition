838cea0c7c6b3c22e90cffa08eee4acd4f3a56e0
Reinaldo J <reinaldo@bloopark.de>
2023-01-21 05:05:04 +0100

    [ADD] Upload single file and store it
    Single file can be sent using the endpoint /upload. It will be saved
    in the uploads folder.

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask
+import logging
 
+from flask import Flask, request
+
+logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
 
@@ -7,6 +10,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return {'message': 'Hello Flask :D'}
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['file']
+    filename = file.filename
+    file.save('./uploads/' + filename)
+    logging.debug(f'File {filename} saved to uploads folder.')
+    return {'message': 'File uploaded.'}
 
 
 if __name__ == '__main__':
